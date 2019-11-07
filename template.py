21ba46ec7958a7162e5515f9f02a4292df34172b
Annette Ringe <annetteringe@gmail.com>
2019-11-07 08:35:52 -0800

    experimented with sys module

--- template.py
+++ template.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
+
+
 def main():  # colon marks the beginning of a block of code
     print("You rock!")
 
+    for item in sys.argv:  # talk about for loop 
+        print(item)
+        
 if __name__ == '__main__':
     main()
