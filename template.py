c55deea974e6dce6aa7777a5ddb75a709acbe714
Annette Ringe <annetteringe@gmail.com>
2019-11-14 09:04:00 -0800

    Ran infinite while loop including task decisions

--- template.py
+++ template.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
 
 
 def main():  # colon marks the beginning of a block of code
-    name = input("What is your name? ")
-    print(f"You rock, {name}!")  # f makes this an f-string a formatted string
-    
-if __name__ == '__main__':
+    while True:
+        task = input("wat? ")
+        if task == "quit":
+            break
+        print(f"Did {task}")
+
+if __name__ == '__main__': # this calls the script from the command line
     main()
