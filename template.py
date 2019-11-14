74892e7cb827554c56ed908304fda3dd7e3ffd2d
Annette Ringe <annetteringe@gmail.com>
2019-11-14 08:29:19 -0800

    script now takes input

--- template.py
+++ template.py
@@ -4,8 +4,8 @@ import sys
 
 
 def main():  # colon marks the beginning of a block of code
-    for name in sys.argv[1:]: # colon in [] is a 'slice' meaning 1 plus the rest of the list   
-        print(f"You rock, {name}!")  # f makes this an f-string a formatted string
-      
+    name = input("What is your name? ")
+    print(f"You rock, {name}!")  # f makes this an f-string a formatted string
+    
 if __name__ == '__main__':
     main()
