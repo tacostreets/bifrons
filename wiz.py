89ae0e44db4003bf8457f85412d8cde7d1c806c8
Annette Ringe <annetteringe@gmail.com>
2019-12-19 08:16:44 -0800

    added math to command line script

---
+++ wiz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def power(num, P):
+    """raises NUM to the exponent P."""
+    return num**P 
+
+if __name__=="__main__":
+    result = power(2, 3)
+    print(result)
+    
