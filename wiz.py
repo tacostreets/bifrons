e80b9632349ce59e534cd41b1cf6091a73d84965
Annette Ringe <annetteringe@gmail.com>
2019-12-19 08:45:48 -0800

    added main while loop with task input cs101 wiz 1

--- wiz.py
+++ wiz.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+def main():
+    while True:
+        task = input("> ")
+        if task == "quit":
+            break
+        print(f"you asked me to:{task}")
+            
 def power(num, P):
     """raises NUM to the exponent P."""
     return num**P 
 
 if __name__=="__main__":
-    result = power(2, 3)
-    print(result)
-    
+    main()
