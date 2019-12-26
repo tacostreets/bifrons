33256c724f7bf69626be20842128d0300f018852
Annette Ringe <annetteringe@gmail.com>
2019-12-26 09:55:55 -0800

    turned it into a game!

--- wiz.py
+++ wiz.py
@@ -5,18 +5,29 @@ def main():
     while True:
         try: 
             task = input("> ")
-            if task == "quit":
-                print("I am now quitting")
-                break
-            elif task == "paradox":
-                1 / 0
-            print(f"you asked me to:{task}")
+            request(task)
+
         except (EOFError, KeyboardInterrupt):
             print("\nEnd of line user")
             break
-        except ZeroDivisionError:
-            print("\nDividing by zero is bad")
-            
+
+def request(task):
+    if task == "tower":
+        print("You travel to your modest one story wizard tower!")
+    elif task == "village":
+        print("You travel to the village 100 yds upwind from your tower.")
+    elif task == "help":
+        print("You can do any of the following:")
+        print("  help")
+        print("  tower")
+        print("  village")
+        print("  quit")
+    elif task == "quit":
+        print("I am now quitting")
+        raise EOFError
+    else:
+        print(f"I don't understand:{task}")
+
 def power(num, P):
     """raises NUM to the exponent P."""
     return num**P 
